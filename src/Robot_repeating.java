febab74f2dee9c1887912a2b81750e4f9d115dfe
Savera <Level 2>
2017-01-25 18:32:50 -0800

    made a robot
    you must speak to the frog named kek

---
+++ src/Robot_repeating.java
@@ -0,0 +1,19 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class Robot_repeating {
+public static void main(String[] args) {
+	Robot kek= new Robot();
+	kek.
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
